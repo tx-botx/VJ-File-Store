0b7efe129e1dfa0e08a0137ca735015f1fae0892
tx-botx <tgbotdeploymail@gmail.com>
2025-11-24 16:38:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,13 +18,13 @@ def is_enabled(value, default):
         return default
       
 # Bot Information
-API_ID = int(environ.get("API_ID", "27166502"))
+API_ID = int(environ.get("API_ID", " 27166502"))
 API_HASH = environ.get("API_HASH", "35b9c34a2b29b20bdae81d82e2863cec")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7545686315:AAF7V88EmrPgbQaYn-623q6kNxn2qhxIgtc")
+BOT_TOKEN = environ.get("BOT_TOKEN", "8456563194:AAHljEVh_dueZu80PlzcWj6z3RMmO56bzi0")
 
 PICS = (environ.get('PICS', 'https://graph.org/file/ed19ee56181e87775c6b9-8d5e39c5992adeaf96.jpg')).split() # Bot Start Picture
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '7012709838').split()]
-BOT_USERNAME = environ.get("BOT_USERNAME", "nudixxhotbot") # without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "Demoq0Bot") # without @
 PORT = environ.get("PORT", "8080")
 
 # Clone Info :-
@@ -35,18 +35,18 @@ CLONE_DB_URI = environ.get("CLONE_DB_URI", "")
 CDB_NAME = environ.get("CDB_NAME", "clonetechvj")
 
 # Database Information
-DB_URI = environ.get("DB_URI", "mongodb+srv://jhoneysins094:jhoneysins094@cluster0.pziho.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_URI = environ.get("DB_URI", "mongodb+srv://adarshrajputx:CxjiTK56oacjwg78@cluster0.72l1y2f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 DB_NAME = environ.get("DB_NAME", "techvjbotz")
 
 # Auto Delete Information
 AUTO_DELETE_MODE = bool(environ.get('AUTO_DELETE_MODE', True)) # Set True or False
 
 # If Auto Delete Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "30")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "1800")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "15")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "900")) # Time in Seconds
 
 # Channel Information
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002646812735"))
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1003454021940"))
 
 # File Caption Information
 CUSTOM_FILE_CAPTION = environ.get("CUSTOM_FILE_CAPTION", f"{script.CAPTION}")
@@ -70,7 +70,7 @@ WEBSITE_URL_MODE = bool(environ.get('WEBSITE_URL_MODE', False)) # Set True or Fa
 WEBSITE_URL = environ.get("WEBSITE_URL", "") # For More Information Check Video On Yt - @Tech_VJ
 
 # File Stream Config
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or False
+STREAM_MODE = bool(environ.get('STREAM_MODE', False)) # Set True or False
 
 # If Stream Mode Is True Then Fill All Required Variable, If False Then Don't Fill.
 MULTI_CLIENT = False
